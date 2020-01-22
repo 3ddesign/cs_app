8ae6efdbc0bb5adf133174bb1b27e61715eb80a0
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-01-22 08:10:01 +0200

    add examples

--- cs_test/Program.cs
+++ cs_test/Program.cs
@@ -83,8 +83,123 @@ namespace cs_test
                 Console.WriteLine(isNullOrWhiteSpace);
             }
 
-            StringEmptiness();
-            StringModification();
+            static void QueryingStrings()
+            {
+                string name = "abracadabra";
+                bool containsA = name.Contains('a');
+                bool containsE = name.Contains('E');
+
+                Console.WriteLine(containsA);
+                Console.WriteLine(containsE);
+
+                bool endsWithAbra = name.EndsWith("abra");
+                Console.WriteLine(endsWithAbra);
+
+                bool startsWithAbra = name.StartsWith("abra");
+                Console.WriteLine(startsWithAbra);
+
+                int indexOfA = name.IndexOf('a', 1);
+                Console.WriteLine(indexOfA);
+
+                int lastIndexOfR = name.LastIndexOf('r');
+                Console.WriteLine(lastIndexOfR);
+
+                Console.WriteLine(name.Length);
+
+                string substrFrom5 = name.Substring(5);
+                string substrFromTo = name.Substring(0, 3);
+
+                Console.WriteLine(substrFrom5);
+                Console.WriteLine(substrFromTo);
+            }
+
+            static void StaticAndInstanceMembers()
+            {
+                string name = "abracadabra";
+                bool containsA = name.Contains('a');
+                bool containsE = name.Contains('E');
+
+                Console.WriteLine(containsA);
+                Console.WriteLine(containsE);
+
+                string abc = string.Concat("a", "b", "c");
+                Console.WriteLine(abc);
+
+                Console.WriteLine(int.MinValue);
+
+                int x = 1;
+                string xStr = x.ToString();
+                Console.WriteLine(xStr);
+                Console.WriteLine(x);
+            }
+
+            static void ComparisonOperators()
+            {
+                int x = 1;
+                int y = 2;
+
+                bool areEqual = x == y;
+                Console.WriteLine(areEqual);
+
+                bool result = x > y;
+                Console.WriteLine(result);
+
+                result = x >= y;
+                Console.WriteLine(result);
+
+                result = x < y;
+                Console.WriteLine(result);
+
+                result = x <= y;
+                Console.WriteLine(result);
+
+                result = x != y;
+
+                Console.WriteLine(result);
+            }
+
+            static void MathOperations()
+            {
+                int x = 1;
+                int y = 2;
+
+                int z = x + y;
+                int k = x - y;
+                int a = z + k - y;
+
+                Console.WriteLine(z);
+                Console.WriteLine(k);
+                Console.WriteLine(a);
+
+                int b = z * 2;
+                int c = k / 2;
+
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+
+                a = 4 % 2;
+                b = 5 % 2;
+
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+
+                a = 3;
+                a = a * a;
+                //a = a * a * a;
+                Console.WriteLine(a);
+
+                a = (2 + 2) * 2;
+                Console.WriteLine(a);
+                a *= 2;
+                //a = a * 2
+                Console.WriteLine(a);
+                a /= 2;
+                //a = a / 2
+                Console.WriteLine(a);
+            }
+
+            //StringEmptiness();
+            //StringModification();
         }
     }
 }
