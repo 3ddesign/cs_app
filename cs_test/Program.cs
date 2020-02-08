5fa35294f71c47241000ed35e6ef7f3bd0b94021
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-02-08 07:41:22 +0200

    add var examples

--- cs_test/Program.cs
+++ cs_test/Program.cs
@@ -327,6 +327,47 @@ namespace cs_test
                 Console.WriteLine('\u0420');
                 Console.WriteLine('\u0421');
             }
+
+            static void Variables()
+            {
+                int x = -1;
+
+                int y;
+                y = 2;
+
+                //Int32 x1 = -1;
+
+                //uint z = -1;
+
+                float f = 1.1f;
+
+                double d = 2.3;
+
+                int x2 = 0;
+                int x3 = new int();
+
+                var a = 1;
+                var b = 1.2;
+
+                //Dictionary<int, string> dict = new Dictionary<int, string>();
+                //var dict = new Dictionary<int, string>();
+
+                //var v;
+
+                decimal money = 3.0m;
+
+                char @char = 'A';
+                string name = "John";
+
+                bool canDrive = true;
+                bool canDraw = false;
+
+                object obj1 = 1;
+                object obj2 = "obj2";
+
+                Console.WriteLine(a);
+                Console.WriteLine(name);
+            }
         }
     }
 }
